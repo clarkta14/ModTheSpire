a3239aaf20f8311497dc284e8eb655201b989721
clarkta14 <54959325+clarkta14@users.noreply.github.com>
2020-12-14 13:46:34 -0500

    Created first simple Attack Card; AcidSplash

---
+++ StS-DefaultModBase-master/theDefault/src/main/java/testMod/cards/AcidSplash.java
@@ -0,0 +1,67 @@
+package testMod.cards;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import testMod.DefaultMod;
+import testMod.characters.TheDefault;
+
+import static testMod.DefaultMod.makeCardPath;
+
+public class AcidSplash extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID("AcidSplash"); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("${NAME}.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;  // COST = ${COST}
+    private static final int UPGRADED_COST = 1; // UPGRADED_COST = ${UPGRADED_COST}
+
+    private static final int DAMAGE = 8;    // DAMAGE = ${DAMAGE}
+    private static final int UPGRADE_PLUS_DMG = 3;  // UPGRADE_PLUS_DMG = ${UPGRADED_DAMAGE_INCREASE}
+
+    // /STAT DECLARATION/
+
+
+    public AcidSplash() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
