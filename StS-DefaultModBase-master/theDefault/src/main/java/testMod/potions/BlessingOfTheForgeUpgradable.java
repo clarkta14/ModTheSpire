7f56ebf462e810868025cf9ca9ede8f1ab902ee6
clarkta14 <54959325+clarkta14@users.noreply.github.com>
2020-12-17 07:25:17 -0500

    Did not need to override getPotency(int)

--- StS-DefaultModBase-master/theDefault/src/main/java/testMod/potions/BlessingOfTheForgeUpgradable.java
+++ StS-DefaultModBase-master/theDefault/src/main/java/testMod/potions/BlessingOfTheForgeUpgradable.java
@@ -72,12 +72,6 @@ public class BlessingOfTheForgeUpgradable extends BlessingOfTheForge implements 
         return false;
     }
 
-    // This is your potency.
-    @Override
-    public int getPotency(final int ascension) {
-        return 1;
-    }
-
     @Override
     public int getPotionLevel() {
         return potionLevel;
